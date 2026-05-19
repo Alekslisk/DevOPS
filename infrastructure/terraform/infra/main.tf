619d5e7cc583c48c7521afe3f9077d5f7ab71b55
alekslisk <panachev.aleksandr.work@gmail.com>
2026-05-19 17:21:22 +0500

    final

--- infrastructure/terraform/infra/main.tf
+++ infrastructure/terraform/infra/main.tf
@@ -7,15 +7,15 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.24.0"
+      version = ">= 2.24.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.12.0"
+      version = ">= 2.12.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.2.0"
+      version = ">= 3.2.0"
     }
   }
 }
