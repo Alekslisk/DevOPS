5b19064e1a4fd6b9f9eff8eaefd382bc380e6a39
alekslisk <panachev.aleksandr.work@gmail.com>
2026-05-19 12:46:35 +0500

    final

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -37,21 +37,12 @@ provider "helm" {
   }
 }
 
-resource "helm_release" "kube_prometheus_stack" {
-  name             = "prometheus-stack"
-  repository       = "https://prometheus-community.github.io/helm-charts"
-  chart            = "kube-prometheus-stack"
-  namespace        = "monitoring"
-  create_namespace = true
-}
-
-
-
 resource "helm_release" "loki" {
   name             = "loki"
   repository       = "https://grafana.github.io/helm-charts"
   chart            = "loki-stack"
   namespace        = "monitoring"
+  create_namespace = true
   
   set {
     name  = "promtail.enabled"
